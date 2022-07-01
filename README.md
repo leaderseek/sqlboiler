11b2702309301739eab9c61940d6557cef05b82a
Lewis Webb <lewisrwebb@gmail.com>
2022-07-01 19:38:55 +0100

    feat: create repository for leaderseek db

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # sqlboiler
 Contains the SQL schema definition and associated boilerplate compiled for Go.
+
+## Setup DB
+- Run `docker compose up`
+- Run `psql -U user1 -f .\sql\schema.sql`
+- Enter password: pass1
+
+## Setup SQLBoiler
+- Run `go install github.com/volatiletech/sqlboiler/v4@latest`.
+- Run `go install github.com/volatiletech/sqlboiler/v4/drivers/sqlboiler-psql@latest`.
+- Check `.\sqlboiler.toml` matches with your database. [See here for help.](https://github.com/volatiletech/sqlboiler#configuration)
+
+## Update Schema
+- Edit `.\sql\schema.sql`
+- [Setup DB](#setup-db)
+- [Setup SQLBoiler](#setup-sqlboiler)
+- Delete `.\repository`
+- Run `sqlboiler psql`
---
+++ sql/schema.sql
@@ -0,0 +1,32 @@
+CREATE SCHEMA leaderseek
+
+CREATE TABLE player (
+    id UUID PRIMARY KEY,
+    email_address TEXT UNIQUE NOT NULL,
+    forenames TEXT,
+    surnames TEXT,
+    mobile_telephone_address TEXT
+)
+
+CREATE TABLE team (
+    id UUID PRIMARY KEY,
+    display_name TEXT UNIQUE NOT NULL
+)
+
+CREATE TABLE team_member (
+    id UUID PRIMARY KEY REFERENCES player,
+    team_id UUID REFERENCES team
+)
+
+CREATE TABLE leader_supporter (
+    id UUID PRIMARY KEY REFERENCES player,
+    candidate_id UUID REFERENCES team_member
+)
+
+CREATE VIEW leader_supporter_count AS (
+    SELECT
+    ls.candidate_id,
+    COUNT(ls.id) AS supporter_count
+    FROM leader_supporter ls
+    GROUP BY ls.candidate_id
+);
